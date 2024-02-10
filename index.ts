6f811db4a741877bd920546277096db35de28509
Sim <brhulla@gmail.com>
2024-02-10 13:02:22 +0100

    initialisation

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import env from "dotenv";
+
+env.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
